22c52970c2e8a16bd73d1d3a286929e542272a84
gpgun0 <rjsgh7943@gmail.com>
2021-11-17 15:53:12 +0900

    1874_스택수열_단축

--- data_structure/solution/1874_스택수열.py
+++ data_structure/solution/1874_스택수열.py
@@ -9,13 +9,7 @@ for i in range(n-1, -1, -1):
 
 i = 1
 while i <= n:
-    if not s1:
-        s1.append(i)
-        i += 1
-        result += "+"
-        continue
-
-    if s1[-1] != s2[-1]:
+    if not s1 or s1[-1] != s2[-1]:
         s1.append(i)
         i += 1
         result += "+"
