dc8231a4d1ffe5c28d8450a5d8a50e5a05305882
gpgun0 <rjsgh7943@gmail.com>
2021-11-20 15:12:47 +0900

    15989 최소 회의실 개수 & 11000 강의실 배정

--- greedy/solution/11000_강의실_배정.py
+++ greedy/solution/11000_강의실_배정.py
@@ -1,14 +1,10 @@
-import sys
 import heapq as hq
-input = sys.stdin.readline
 t = [list(map(int, input().split())) for _ in range(int(input()))]
 t.sort(key = lambda x: x[0])
-
 rooms = [0]
 for s, e in t:
     et = rooms[0]
     if et <= s:
         hq.heappop(rooms)
     hq.heappush(rooms, e)
-
 print(len(rooms))
