6c2d016d72519b16f9e520369bb0bb441df50a07
Kim-GeonHo <rjsgh7943@gist.ac.kr>
2021-11-22 23:53:07 +0900

    1823 수확 재귀

---
+++ dynamic_programming_1/solution/1823_수확.py
@@ -0,0 +1,16 @@
+import sys
+sys.setrecursionlimit(10000)
+def get_benefit(i, j, order):
+  order += 1
+
+  if j-i == 1:
+    return max(v[i]*order + v[j]*(order+1), v[i]*(order+1) + v[j]*order)
+  
+  return max(get_benefit(i, j-1, order) + j*order, get_benefit(i+1, j, order) + i*order)
+
+n = int(input())
+v = [0] * n
+for i in range(n):
+  v[i] = int(input())
+
+print(get_benefit(0, n-1, 0))
